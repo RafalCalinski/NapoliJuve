1a292f1a6b1752697fb077eca22b7672b057c29f
RafalCalinski <kakti.gamer@gmail.com>
2023-05-29 16:46:13 +0200

    .

--- Objects/player_movement.gd
+++ Objects/player_movement.gd
@@ -1,11 +1,12 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-const max_speed = 10
-const acceleration = 50
-const friction = 15
+
+
+const max_speed = 500
+const acceleration = 500
+const friction = 500
 
 var input = Vector2.ZERO
-var velocity = Vector2.ZERO
 
 func get_input():
 	input.x = int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left"))
@@ -29,6 +30,16 @@ func player_movement(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	
+	
+	
 	player_movement(delta)
-	move_and_collide(velocity)
+	move_and_slide()
 	look_at(get_global_mouse_position())
+	
+	
+func _unhandled_input(event: InputEvent):
+	if event.is_action_released("shoot"):
+		shoot()
+		
+func shoot():
+	print("shoot")
