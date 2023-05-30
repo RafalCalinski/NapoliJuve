2f12ab91b95560a38cdcad002d46169dd55dccfd
Technicman69 <110126449+Technicman69@users.noreply.github.com>
2023-05-30 18:56:00 +0200

    Added crate destroy

---
+++ Objects/crate.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_hitbox_area_entered(area):
+	$AnimationPlayer.play("destroy")
+	$Hitbox.monitoring = false
+	$AudioStreamPlayer.play()
+
+
+#func _on_animation_player_animation_finished(anim_name):
+#	queue_free()
+
+
+func _on_audio_stream_player_finished():
+	queue_free()
