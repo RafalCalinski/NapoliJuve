d5d3075a1769cb02299e2637c4bb7afd290c8fb8
RafalCalinski <kakti.gamer@gmail.com>
2023-05-30 20:50:59 +0200

    Niszczenie drzew

--- Objects/tree.gd
+++ Objects/tree.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+var HP = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,15 +13,16 @@ func _process(delta):
 
 
 func _on_hitbox_area_entered(area):
-	$AnimationPlayer.play("destroy")
-	$Hitbox.queue_free()
-	$CollisionShape2D.queue_free()
-	$AudioStreamPlayer.play()
+	HP -= 1
+	$AnimationPlayer.play("hit")
+	if HP <= 0:
+		$AnimationPlayer.play("destroy")
+		$Hitbox.queue_free()
+		$CollisionShape2D.queue_free()
+		$AudioStreamPlayer.play()
 
 
 #func _on_animation_player_animation_finished(anim_name):
 #	queue_free()
-
-
 func _on_audio_stream_player_finished():
 	queue_free()
